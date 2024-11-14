3b60d655acc15decc98cc353e8d236669ced5418
EugeniaSQ <eugeq2005@gmail.com>
2024-11-14 03:43:14 -0300

    Add files via upload

--- MENUJAVA.js
+++ MENUJAVA.js
@@ -1,10 +1,10 @@
 function MiFuncion() {
-    // aquí se define el ID del elemento que va a tomar la clase
-  var x = document.getElementsByClassName("navegador");
-  if (x.className === "menudebarra") {
-      // esta es la clase que se agrega al elemento con el id="navegador"
-    x.className += "responsive";
-  } else {
-    x.className = "menudebarra";
-  }
+  // aquí se define el ID del elemento que va a tomar la clase
+var x = document.getElementById("navegador");
+if (x.className === " ") {
+    // esta es la clase que se agrega al elemento con eo id="navegador"
+  x.className += "responsive";
+} else {
+  x.className = " ";
+}
 }
